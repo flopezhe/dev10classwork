96125cc7077fe33995a4a41503c3040c030a3810
Farid Lopez <faridlopez5@gmail.com>
2024-06-03 13:56:00 -0700

    completed exercise07

--- week-01/exercises/variables-and-types-exercises/src/Exercise07.java
+++ week-01/exercises/variables-and-types-exercises/src/Exercise07.java
@@ -5,5 +5,11 @@ public class Exercise07 {
         // where each floor has 15 "rows" and 8 "columns" of offices.
         // 2. Use whatever approach you think is best.
         // 3. Print the result.
+        int buildingStories = 23;
+        int buildingRows = 15;
+        int buildingColumns = 8;
+        int numberOfOffices = buildingStories * buildingRows * buildingColumns;
+
+        System.out.println(numberOfOffices);
     }
 }
