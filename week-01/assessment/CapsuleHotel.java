92bdb86dc121dfbc1c8c40db4528a8511ff77a15
Farid Lopez <faridlopez5@gmail.com>
2024-06-07 16:40:25 -0700

    Finished method 2 and updated basic layout to implement both methods

--- week-01/assessment/CapsuleHotel.java
+++ week-01/assessment/CapsuleHotel.java
@@ -1,16 +1,13 @@
-import java.lang.reflect.Array;
-import java.util.Arrays;
 import java.util.Scanner;
 public class CapsuleHotel {
     public static void main(String[] args) {
         String welcome = "\"Welcome to the Capsule Hotel\"";
         System.out.println(welcome);
         System.out.println("=".repeat(welcome.length()));
-        System.out.println("Press Enter to continue");
         Scanner console = new Scanner(System.in);
         String[] hotelBuilding = new String[100];
 
-        String selection = console.nextLine();
+//        String selection = console.nextLine();
 
         String input;
 
@@ -24,10 +21,16 @@ public class CapsuleHotel {
             input = console.nextLine();
             if (input.equals("1")) {
                 addGuest(console, hotelBuilding);
-                displayCapsuleHotelBuilding(hotelBuilding);
-                break;
             } else if(input.equals("2")){
                 System.out.println("Great, lets checkout the guest");
+                System.out.println("Please enter the room number of the guest:");
+                int roomNum = Integer.parseInt(console.nextLine());
+                if(roomNum <= 0 || roomNum >= hotelBuilding.length){
+                    return;
+                } else {
+                    System.out.println("Great Room#"+roomNum+" has been checked out, you're all set!");
+                    hotelBuilding[roomNum-1] = "[Empty Room]";
+                }
             } else if(input.equals("3")){
                 displayCapsuleHotelBuilding(hotelBuilding);
             } else if (input.equals("4")){
@@ -42,10 +45,15 @@ public class CapsuleHotel {
     }
 
     public static void addGuest(Scanner console, String[] capsuleHotelBuilding){
-        System.out.println("Great lets book a guest, please enter the guest name:");
+
         System.out.println("Enter the room number you want to add the guest to: ");
         int room = Integer.parseInt(console.nextLine());
-        if(room < 0 || )
+        if(room <= 0 || room >= capsuleHotelBuilding.length){
+            return;
+        }
+        System.out.println("Guest Name:");
+        String guestName = console.nextLine();
+        capsuleHotelBuilding[room-1] = guestName;
     }
     public static void displayCapsuleHotelBuilding(String[] capsuleHotelBuilding){
         System.out.println("The total current occupancy is below: ");
