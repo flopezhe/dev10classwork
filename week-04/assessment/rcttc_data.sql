51f70f6ebecc1dd598f30d15ddba781046bd9e0a
Farid Lopez <faridlopez5@gmail.com>
2024-06-29 18:43:43 -0700

    finished, completed dml and query exercises

--- week-04/assessment/rcttc_data.sql
+++ week-04/assessment/rcttc_data.sql
@@ -46,6 +46,50 @@ inner join tiny_theaters.customer c on c.last_name  = i.customer_last
 inner join tiny_theaters.`show` s on s.show_name = i.`show` and s.show_date = i.`date`;
 
 
+use tiny_theaters;
+
+update `show` set 
+	show_price = 22.25
+where show_id = 5;
+
+update customer set 
+	phone_number = '1-801-EAT-CAKE'
+where customer_id = 48;
+
+-- update seats for 2 customers to sit together
+-- ticket id 98 to B4 ticket ID 99(C1) OR 100 (B4) TO C2 TICKET ID 101 TO A4
+update tickets set 
+ticket_seat = 'B4'
+where ticket_id = 98;
+
+update tickets set 
+ticket_seat = 'C2'
+where ticket_id = 100;
+
+update tickets set 
+ticket_seat = 'A4'
+where ticket_id = 101;
+
+-- delete single reservation tix
+-- single reservation customer id 7 8 10 15 18 19 22 25 26 
+-- ticket id 25 26 29 41 50 51 59 67 68
+delete from tickets 
+where ticket_id in (25, 26, 29,41, 50, 51, 59, 67, 68);
+
+-- delete liv fake customer
+-- customer id 65
+-- ticket id 173 174 
+delete from tickets 
+where ticket_id in (173,174);
+
+delete from customer
+where customer_id = 65;
+
+
+
+
+
+
     
 
 
--- week-04/assessment/rcttc_queries.sql
+++ week-04/assessment/rcttc_queries.sql
@@ -1,14 +1,157 @@
--- QUERIES
+-- QUERIES FOR DML ONLY
 use rcttc;
 
 select
-`seat` 
-from `rcttc_import`;
+*
+from rcttc_import;
 
 use tiny_theaters;
 
 select 
-ticket_seat
+*
 from tickets;
+
+select *
+from `show`
+where theater_id = 1;
+-- show id 1 send in the clowns 2024 03 01
+-- show id 2 send in the clowns 2024 09 21
+-- show id 3 the dress 2024 01 05 
+-- show id 4 tell me what to think 2024 12-21
+
+-- Error Code: 1055. Expression #1 of SELECT list is not in GROUP BY clause and contains nonaggregated column 'tiny_theaters.tickets.ticket_id' which is not functionally dependent on columns in GROUP BY clause; this is incompatible with sql_mode=only_full_group_by
+
+select 
+*
+from tickets 
+where show_id in (1,2,3,4);
+-- single reservation customer id 7 8 10 15 18 19 22 25 26 
+-- ticket id 25 26 29 41 50 51 59 67 68
+
+select 
+*
+from theater;
+-- theater id is 1
+
+select
+*
+from customer
+where first_name = 'Liv';
+
+select 
+*
+from tickets
+where customer_id = 65;
+
+-- QUERIES for QUERY SECTION
+
+select 
+*
+from `show`
+where date(show_date) >='2024-10-01'
+and date (show_date) <= '2024-12-31';
+
+select *
+from customer;
+
+select
+*
+from customer
+where email_address like '%.com';
+
+
+
+select
+* 
+from `show`
+order by show_price asc
+limit 3;
+
+-- Error Code: 1146. Table 'tiny_theaters.ticket' doesn't exist
+
+select distinct
+c.first_name,
+c.customer_id,
+s.show_name
+from customer c
+inner join tickets t on c.customer_id = t.customer_id
+inner join `show` s on t.show_id = s.show_id;
+
+select 
+c.first_name,
+t.ticket_seat,
+s.show_name,
+th.theater_name
+from customer c
+inner join tickets t on c.customer_id = t.customer_id
+inner join `show` s on t.show_id = s.show_id
+inner join theater th on s.theater_id = th.theater_id;
+
+select
+*
+from customer
+where `home_address` = '';
+
+select 
+*
+from customer c
+inner join tickets t on c.customer_id = t.customer_id
+inner join `show` s on t.show_id = s.show_id
+inner join theater th on s.theater_id = th.theater_id;
+
+select distinct
+c.first_name,
+c.customer_id,
+count(t.ticket_id)
+from customer c
+inner join tickets t on c.customer_id = t.customer_id
+group by customer_id;
+
+
+-- count by show
+select
+s.show_id,
+s.show_name,
+sum(s.show_price) as total_revenue
+from tickets t
+inner join `show` s on t.show_id = s.show_id
+group by s.show_id, s.show_name;
+-- by theater
+select
+th.theater_name,
+sum(s.show_price) as total_revenue
+from tickets t
+inner join `show` s on t.show_id = s.show_id
+inner join theater th on s.theater_id = th.theater_id
+group by th.theater_id;
+
+-- customer who spent the most
+select distinct
+c.first_name,
+c.last_name,
+c.customer_id,
+sum(s.show_price) as total_spent
+from customer c
+inner join tickets t on c.customer_id = t.customer_id
+inner join `show` s on t.show_id= s.show_id
+group by customer_id 
+order by total_spent desc
+limit 1;
+
+-- who spent the most in 2024
+select distinct
+c.first_name,
+sum(s.show_price) as total_spent
+from customer c
+inner join tickets t on c.customer_id = t.customer_id
+inner join `show` s on t.show_id= s.show_id
+where date(s.show_date) >='2024-01-01'
+and date (s.show_date) <= '2024-12-31'
+group by c.customer_id
+order by total_spent desc
+limit 1;
+
+
+
 
 
